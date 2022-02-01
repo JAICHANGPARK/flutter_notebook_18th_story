0c091c8e358950ba0e3d18e55a873cc088672f68
Dreamwalker <aristojeff@gmail.com>
2022-02-01 16:37:56 +0900

    Update start_screen.dart

--- lib/ep1051_travel_car_rental_app/src/ui/start_screen.dart
+++ lib/ep1051_travel_car_rental_app/src/ui/start_screen.dart
@@ -8,7 +8,18 @@ class StartScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-
+          Expanded(child: Stack()),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Row(
+                children: [],
+              ),
+            ),
+          )
         ],
       ),
     );
