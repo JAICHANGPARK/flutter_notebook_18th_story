bfa3147da01983df97e07ae65da4a150b4f1f12d
Dreamwalker <aristojeff@gmail.com>
2022-02-01 16:36:09 +0900

    Create start_screen.dart

---
+++ lib/ep1051_travel_car_rental_app/src/ui/start_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
