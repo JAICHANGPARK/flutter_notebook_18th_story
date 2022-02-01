56ce94d7c8bdff5a065e493bed1a0881b9c760c2
Dreamwalker <aristojeff@gmail.com>
2022-02-01 16:42:23 +0900

    Update start_screen.dart

--- lib/ep1051_travel_car_rental_app/src/ui/start_screen.dart
+++ lib/ep1051_travel_car_rental_app/src/ui/start_screen.dart
@@ -6,39 +6,42 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(child: Stack()),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 32, left: 16, right: 16, top: 24),
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 12,
-              ),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(24),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    "Get Started",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
+      body: SafeArea(
+        top: true,
+        child: Column(
+          children: [
+            Expanded(
+                child: Stack(
+              children: [],
+            )),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 32, left: 16, right: 16, top: 24),
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 12,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Get Started",
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
                     ),
-                  ),
-                  Icon(
-                    Icons.arrow_forward,
-                    color: Colors.white,
-                  )
-                ],
+                    Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
