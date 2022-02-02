191767d31df9eca83ba16bca0a62d13edc7da6e5
Dreamwalker <aristojeff@gmail.com>
2022-02-02 19:01:30 +0900

    Update rental_main_page.dart

--- lib/ep1051_travel_car_rental_app/src/ui/rental_main_page.dart
+++ lib/ep1051_travel_car_rental_app/src/ui/rental_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class RentalMainPage extends StatefulWidget {
   const RentalMainPage({Key? key}) : super(key: key);
 
@@ -13,7 +12,71 @@ class _RentalMainPageState extends State<RentalMainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(24)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [Text("Hi, Dreamwalker"), Spacer(), CircleAvatar()],
+                  ),
+                  Container(
+                    height: 48,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  )
+                ],
+              ),
+            )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 64,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Row(
+                        children: [],
+                      ),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.my_location,
+                            color: Colors.grey,
+                          )),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.my_location,
+                            color: Colors.grey,
+                          )),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.settings),
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
