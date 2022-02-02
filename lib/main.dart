fb75984b35888993254136319f58ae34e9ef7fdb
Dreamwalker <aristojeff@gmail.com>
2022-02-02 18:54:29 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'ep1051_travel_car_rental_app/travel_car_rental_app.dart';
 
-
 main() {
-  runApp(const ProviderScope(child: TravelCarRentalApp()));
+  runApp(ProviderScope(child: TravelCarRentalApp()));
 }
