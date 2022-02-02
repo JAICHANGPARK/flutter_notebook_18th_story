3b5abf37b533aca06227adcdc5ab74e4061646e3
Dreamwalker <aristojeff@gmail.com>
2022-02-02 18:49:50 +0900

    Update travel_car_rental_app.dart

--- lib/ep1051_travel_car_rental_app/travel_car_rental_app.dart
+++ lib/ep1051_travel_car_rental_app/travel_car_rental_app.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import 'src/ui/start_screen.dart';
 
 
+
 class TravelCarRentalApp extends StatelessWidget {
-  const TravelCarRentalApp({Key? key}) : super(key: key);
+  TravelCarRentalApp({Key? key}) : super(key: key);
+  final _router = GoRouter(routes: [
+    GoRoute(
+      path: "/",
+      builder: 
+    )
+  ]);
+
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+
       home: StartScreen(),
     );
   }
