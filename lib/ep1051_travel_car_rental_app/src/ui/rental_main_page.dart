493510243b65925c90d527d1b2ff20c2595bafa2
Dreamwalker <aristojeff@gmail.com>
2022-02-02 18:49:43 +0900

    Create rental_main_page.dart

---
+++ lib/ep1051_travel_car_rental_app/src/ui/rental_main_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class RentalMainPage extends StatefulWidget {
+  const RentalMainPage({Key? key}) : super(key: key);
+
+  @override
+  _RentalMainPageState createState() => _RentalMainPageState();
+}
+
+class _RentalMainPageState extends State<RentalMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(),
+    );
+  }
+}
