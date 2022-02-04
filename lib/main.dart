a35f1097055519eba27ab0d4fddd988de1dc4f9e
Dreamwalker <aristojeff@gmail.com>
2022-02-04 20:40:56 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'ep1051_travel_car_rental_app/travel_car_rental_app.dart';
+import 'ep1054_food_delivery_app/food_delivery_app.dart';
 
 main() {
-  runApp(ProviderScope(child: TravelCarRentalApp()));
+  runApp(ProviderScope(child: FoodDeliveryApp()));
 }
