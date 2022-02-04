cdae9b586866bd576154a770e84c53b6c4ffdbba
Dreamwalker <aristojeff@gmail.com>
2022-02-04 20:41:44 +0900

    Update food_delivery_app.dart

--- lib/ep1054_food_delivery_app/food_delivery_app.dart
+++ lib/ep1054_food_delivery_app/food_delivery_app.dart
@@ -5,6 +5,8 @@ class FoodDeliveryApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: FoodDeliveryHomeScreen(),
+    );
   }
 }
