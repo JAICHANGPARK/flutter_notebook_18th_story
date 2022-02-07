3e372296f21318acc01026b5743890a0a8fe0db8
Dreamwalker <aristojeff@gmail.com>
2022-02-07 20:30:32 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'ep1051_travel_car_rental_app/travel_car_rental_app.dart';
-import 'ep1054_food_delivery_app/food_delivery_app.dart';
+import 'ep1057_dating_app/dating_app.dart';
 
 main() {
-  runApp(ProviderScope(child: FoodDeliveryApp()));
+  runApp(
+    const ProviderScope(
+      child: DatingApp(),
+    ),
+  );
 }
