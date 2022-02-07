62a129694655e7874936f087e3e0fec8da27df46
Dreamwalker <aristojeff@gmail.com>
2022-02-07 20:32:22 +0900

    Update dating_app_main_page.dart

--- lib/ep1057_dating_app/src/ui/dating_app_main_page.dart
+++ lib/ep1057_dating_app/src/ui/dating_app_main_page.dart
@@ -3,11 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final daTabProvider = StateProvider<int>((ref)=>0);
 
-class DatingAppMainPage extends StatelessWidget {
+class DatingAppMainPage extends ConsumerWidget {
   const DatingAppMainPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final tabIndex = ref.watch(daTabProvider);
     return Scaffold(
       body: Column(
         children: [
