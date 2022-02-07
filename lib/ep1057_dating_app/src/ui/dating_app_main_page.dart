77f6659d465a1e7ee626d1264f6e77a184307120
Dreamwalker <aristojeff@gmail.com>
2022-02-07 20:31:22 +0900

    Create dating_app_main_page.dart

---
+++ lib/ep1057_dating_app/src/ui/dating_app_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DatingAppMainPage extends StatelessWidget {
+  const DatingAppMainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
