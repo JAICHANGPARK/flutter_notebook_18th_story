6e0c761336cc941e86826cf377aad32f6eb6ca95
Dreamwalker <aristojeff@gmail.com>
2022-02-09 19:53:44 +0900

    Create fake_face.g.dart

---
+++ lib/ep1057_dating_app/src/model/fake_face.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fake_face.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_FakeFace _$$_FakeFaceFromJson(Map<String, dynamic> json) => _$_FakeFace(
+      age: json['age'] as int?,
+      dateAdded: json['dateAdded'] as String?,
+      fileName: json['fileName'] as String?,
+      imageUrl: json['imageUrl'] as String?,
+    );
+
+Map<String, dynamic> _$$_FakeFaceToJson(_$_FakeFace instance) =>
+    <String, dynamic>{
+      'age': instance.age,
+      'dateAdded': instance.dateAdded,
+      'fileName': instance.fileName,
+      'imageUrl': instance.imageUrl,
+    };
