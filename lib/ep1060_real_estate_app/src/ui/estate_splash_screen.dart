20cbf2a756fc60c431ace92ebbe2cc22c8331ca5
Dreamwalker <aristojeff@gmail.com>
2022-02-10 17:33:08 +0900

    Update estate_splash_screen.dart

--- lib/ep1060_real_estate_app/src/ui/estate_splash_screen.dart
+++ lib/ep1060_real_estate_app/src/ui/estate_splash_screen.dart
@@ -8,28 +8,71 @@ class EstateSplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: CachedNetworkImageProvider(
-              "https://cdn.pixabay.com/photo/2014/07/21/19/20/lobby-398845_960_720.jpg",
+              "https://cdn.pixabay.com/photo/2020/03/23/07/59/office-4959785_960_720.jpg",
             ),
             fit: BoxFit.cover,
           ),
         ),
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.only(left: 16, right: 16, bottom: 48, top: 24),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 120,
               ),
-              Text("Find your dream"),
-              Text("apartment now"),
+              Text(
+                "Find your dream",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "apartment now",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               SizedBox(
-                height: 24,
+                height: 32,
               ),
               Text("Find your place easly travel"),
               Text("anywhere you want with us."),
+              Spacer(),
+              Row(
+                children: [
+                  Container(
+                    height: 3,
+                    width: 38,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(width: 4,),
+                  Container(
+                    height: 3,
+                    width: 38,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                  SizedBox(width: 4,),
+                  Container(
+                    height: 3,
+                    width: 38,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
