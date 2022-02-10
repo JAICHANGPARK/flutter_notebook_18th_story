d226a4c32835330694fe9731f917ad814a39d35a
Dreamwalker <aristojeff@gmail.com>
2022-02-10 17:24:34 +0900

    Update real_estate_app.dart

--- lib/ep1060_real_estate_app/real_estate_app.dart
+++ lib/ep1060_real_estate_app/real_estate_app.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_18th_story/ep1060_real_estate_app/src/ui/estate_splash_screen.dart';
+import 'package:go_router/go_router.dart';
 
 class RealEstateApp extends StatelessWidget {
-  const RealEstateApp({Key? key}) : super(key: key);
+  RealEstateApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp.router(
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
+    );
   }
+
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const EstateSplashScreen(),
+      ),
+    ],
+  );
 }
