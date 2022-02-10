303487b4853323e2ec1843bd30c14fc314797efa
Dreamwalker <aristojeff@gmail.com>
2022-02-10 17:19:41 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'ep1057_dating_app/dating_app.dart';
+import 'ep1060_real_estate_app/real_estate_app.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: DatingApp(),
+      child: RealEstateApp(),
     ),
   );
 }
