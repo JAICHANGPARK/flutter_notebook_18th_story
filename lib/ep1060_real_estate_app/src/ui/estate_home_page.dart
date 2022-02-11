129670bc5807fe178e310b68abd87b0c2cbec5de
Dreamwalker <aristojeff@gmail.com>
2022-02-11 20:02:09 +0900

    Update estate_home_page.dart

--- lib/ep1060_real_estate_app/src/ui/estate_home_page.dart
+++ lib/ep1060_real_estate_app/src/ui/estate_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
 
 class EstateHomePage extends StatefulWidget {
@@ -11,6 +12,24 @@ class EstateHomePage extends StatefulWidget {
 class _EstateHomePageState extends State<EstateHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Good morning"),
+                  Text("Dreamwalker"),
+                ],
+              ),
+              IconButton(onPressed: (){}, icon: Icon(Ionicons.alert_outline))
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
