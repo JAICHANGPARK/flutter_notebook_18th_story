76039bd4cb8ed42fcee3e4dfb6c2e214f4271d31
Dreamwalker <aristojeff@gmail.com>
2022-02-11 19:59:44 +0900

    Create estate_home_page.dart

---
+++ lib/ep1060_real_estate_app/src/ui/estate_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EstateHomePage extends StatefulWidget {
+  const EstateHomePage({Key? key}) : super(key: key);
+
+  @override
+  _EstateHomePageState createState() => _EstateHomePageState();
+}
+
+class _EstateHomePageState extends State<EstateHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
