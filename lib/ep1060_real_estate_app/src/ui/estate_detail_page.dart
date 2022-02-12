d38a4ac60dedbe1d483d517f8114cd3ca4982eac
Dreamwalker <aristojeff@gmail.com>
2022-02-12 16:32:15 +0900

    Update estate_detail_page.dart

--- lib/ep1060_real_estate_app/src/ui/estate_detail_page.dart
+++ lib/ep1060_real_estate_app/src/ui/estate_detail_page.dart
@@ -1,5 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_lorem/flutter_lorem.dart';
+import 'package:ionicons/ionicons.dart';
 
 class EstateDetailPage extends StatefulWidget {
   final String img;
@@ -27,6 +30,7 @@ class _EstateDetailPageState extends State<EstateDetailPage> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Expanded(
                 child: Container(
@@ -43,10 +47,86 @@ class _EstateDetailPageState extends State<EstateDetailPage> {
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Text("${widget.title}", style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20
-                ),),
+                child: Text(
+                  "${widget.title}",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+              Text(
+                "${lorem(paragraphs: 1)}",
+                overflow: TextOverflow.ellipsis,
+                maxLines: 3,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                child: Row(
+                  children: [
+                    CircleAvatar(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Dreamwalker"),
+                          Text("Property owner"),
+                        ],
+                      ),
+                    ),
+                    Spacer(),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.brown[100],
+                      ),
+                      padding: EdgeInsets.all(8),
+                      child: Icon(
+                        Ionicons.chatbubble_outline,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "${widget.price}",
+                          style: TextStyle(
+                            color: Colors.brown,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        TextSpan(
+                          text: " /month",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 64,
+                      decoration: BoxDecoration(
+                        color: Colors.brown,
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Book now",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               )
             ],
           ),
