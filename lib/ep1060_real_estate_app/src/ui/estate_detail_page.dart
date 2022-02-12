0051459e03be9b35d19eef8dff8f12569492a5f5
Dreamwalker <aristojeff@gmail.com>
2022-02-12 16:24:23 +0900

    Update estate_detail_page.dart

--- lib/ep1060_real_estate_app/src/ui/estate_detail_page.dart
+++ lib/ep1060_real_estate_app/src/ui/estate_detail_page.dart
@@ -1,13 +1,18 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class EstateDetailPage extends StatefulWidget {
   final String img;
   final String price;
+  final String title;
+  final String imgTag;
 
   EstateDetailPage({
     Key? key,
     required this.img,
+    required this.title,
     required this.price,
+    required this.imgTag,
   }) : super(key: key);
 
   @override
@@ -18,10 +23,34 @@ class _EstateDetailPageState extends State<EstateDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                      image: CachedNetworkImageProvider(
+                        widget.img,
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text("${widget.title}", style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+                ),),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
