af02b28049e08ac8f2257f25c835bae1a818c98d
Dreamwalker <aristojeff@gmail.com>
2022-02-13 16:09:25 +0900

    Create tutor_login_page.dart

---
+++ lib/ep1063_tutor_app/src/ui/tutor_login_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TutorLoginPage extends StatelessWidget {
+  const TutorLoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                
+              ],
+            ),
+          ),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          ))
+        ],
+      ),
+    );
+  }
+}
