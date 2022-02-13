5d746cbab3c4f304b8834403e1c2d58d76bd6451
Dreamwalker <aristojeff@gmail.com>
2022-02-13 16:14:50 +0900

    Update tutor_login_page.dart

--- lib/ep1063_tutor_app/src/ui/tutor_login_page.dart
+++ lib/ep1063_tutor_app/src/ui/tutor_login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class TutorLoginPage extends StatelessWidget {
   const TutorLoginPage({Key? key}) : super(key: key);
@@ -12,14 +13,38 @@ class TutorLoginPage extends StatelessWidget {
           Expanded(
             child: Stack(
               children: [
-                
+                Positioned(
+                  left: -200,
+                  top: -250,
+                  right: -64,
+                  bottom: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.green[200],
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Positioned(child: SvgPicture.asset("assets/ep1063/learning_bro.svg",),
+                left: 0,
+                  right: 0,
+                  bottom: 0,
+                  top: 64,
+                )
               ],
             ),
           ),
           Expanded(
               child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [],
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Find Your Best"),
+              Text.rich(TextSpan(children:
+              [
+                TextSpan()
+
+              ]))
+            ],
           ))
         ],
       ),
