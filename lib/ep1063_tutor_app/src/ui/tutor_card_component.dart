51113d3e52fdd7072b21cc13fa27a0f39e0e6156
Dreamwalker <aristojeff@gmail.com>
2022-02-15 20:15:24 +0900

    Create tutor_card_component.dart

---
+++ lib/ep1063_tutor_app/src/ui/tutor_card_component.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class TutorCardComponent extends StatelessWidget {
+  const TutorCardComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card();
+  }
+}
