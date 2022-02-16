ac84201921d2659fffcf7e8825d86c8e01fab3a8
Dreamwalker <aristojeff@gmail.com>
2022-02-16 19:48:31 +0900

    Update task_management_app.dart

--- lib/ep1066_task_mng_app/task_management_app.dart
+++ lib/ep1066_task_mng_app/task_management_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'src/ui/task_management_main_page.dart';
+
 
 class TaskManagementApp extends StatelessWidget {
   const TaskManagementApp({Key? key}) : super(key: key);
@@ -7,7 +9,7 @@ class TaskManagementApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: TaskManagementMainPage(),
     );
   }
 }
