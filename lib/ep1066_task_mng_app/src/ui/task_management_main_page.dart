7aa4ce23c42f9f93b7cf9e9f7e308059029ac2e6
Dreamwalker <aristojeff@gmail.com>
2022-02-16 19:53:23 +0900

    Update task_management_main_page.dart

--- lib/ep1066_task_mng_app/src/ui/task_management_main_page.dart
+++ lib/ep1066_task_mng_app/src/ui/task_management_main_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TaskManagementMainPage extends StatefulWidget {
@@ -11,10 +12,38 @@ class _TaskManagementMainPageState extends State<TaskManagementMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  CircleAvatar(),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Dreamwalker"),
+                        Text("Hello, Dreamwalker 👋"),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)
+                    ),
+                    padding: EdgeInsets.all(4),
+                    child: Icon(Icons.menu),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
