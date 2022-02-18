cd28d5c9bcbdc2499a041f5f55c6cc2a8b20b7c1
Dreamwalker <aristojeff@gmail.com>
2022-02-18 20:37:55 +0900

    Create task_tab.dart

---
+++ lib/ep1066_task_mng_app/src/model/task_tab.dart
@@ -0,0 +1,14 @@
+
+
+class TaskTab{
+  String title;
+
+  TaskTab(this.title);
+}
+
+List<TaskTab> tabItems = [
+  TaskTab("Backlog"),
+  TaskTab("To Do"),
+  TaskTab("In Progress"),
+  TaskTab("Completed"),
+];
