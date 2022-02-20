236f8d24d04ae01ecc96b5e0b8afa93296230f8a
Dreamwalker <aristojeff@gmail.com>
2022-02-20 17:56:18 +0900

    Create treadmill_app.dart

---
+++ lib/ep1070_treadmill_app/treadmill_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class TreadmillApp extends StatelessWidget {
+  const TreadmillApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
