58800530910d7a7001d0c4b235219cbf29c6b595
Dreamwalker <aristojeff@gmail.com>
2022-02-20 17:57:06 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'ep1066_task_mng_app/task_management_app.dart';
+import 'package:flutter_notebook_18th_story/ep1070_treadmill_app/treadmill_app.dart';
+
 
 main() {
   runApp(
      const ProviderScope(
-      child: TaskManagementApp(),
+      child: TreadmillApp(),
     ),
   );
 }
