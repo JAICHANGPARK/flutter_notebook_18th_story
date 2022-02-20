2e43d285eeb36d23618f986e6c8e077f5ee87dbd
Dreamwalker <aristojeff@gmail.com>
2022-02-20 18:17:11 +0900

    Update treadmill_home_page.dart

--- lib/ep1070_treadmill_app/src/ui/treadmill_home_page.dart
+++ lib/ep1070_treadmill_app/src/ui/treadmill_home_page.dart
@@ -6,90 +6,128 @@ class TreadmillHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Expanded(
-            child: Placeholder(),
-          ),
-          Container(
-            height: 180,
-            decoration: const BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(36),
-                topLeft: Radius.circular(36),
-              ),
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 36, vertical: 36),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    CircleAvatar(
-                      radius: 24,
-                      backgroundColor: Colors.white,
-                      foregroundColor: Colors.black,
-                      child: Icon(Icons.power_settings_new),
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      "SLEEP",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 10,
-                      ),
-                    )
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    CircleAvatar(
-                      radius: 36,
-                      backgroundColor: Colors.white,
-                      foregroundColor: Colors.black,
-                      child: Text(
-                        "START",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Hi Dreamwalker"),
+                        CircleAvatar(),
+                      ],
                     ),
                     SizedBox(
                       height: 24,
-                    )
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    CircleAvatar(
-                      radius: 24,
-                      backgroundColor: Colors.white,
-                      foregroundColor: Colors.black,
-                      child: Text("A"),
                     ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      "AUTO",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 10,
+                    Expanded(
+                        child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[100],
+                            ),
+                            height: 200,
+                          )
+                        ],
                       ),
-                    )
+                    ))
                   ],
                 ),
-              ],
+              ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 180,
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(36),
+                  topLeft: Radius.circular(36),
+                ),
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 36,
+                vertical: 36,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      CircleAvatar(
+                        radius: 24,
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        child: Icon(Icons.power_settings_new),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "SLEEP",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10,
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      CircleAvatar(
+                        radius: 36,
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        child: Text(
+                          "START",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24,
+                      )
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      CircleAvatar(
+                        radius: 24,
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        child: Text("A"),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "AUTO",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
