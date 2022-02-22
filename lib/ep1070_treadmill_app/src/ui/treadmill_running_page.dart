3587c30912b9e16561172f8725282a49bb117476
Dreamwalker <aristojeff@gmail.com>
2022-02-22 20:05:31 +0900

    Update treadmill_running_page.dart

--- lib/ep1070_treadmill_app/src/ui/treadmill_running_page.dart
+++ lib/ep1070_treadmill_app/src/ui/treadmill_running_page.dart
@@ -11,7 +11,35 @@ class _TreadmillRunningPageState extends State<TreadmillRunningPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                flex: 5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "56:12",
+                      style: TextStyle(
+                        fontSize: 64,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Time",
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                )),
+            Expanded(flex: 5, child: Placeholder()),
+            Expanded(flex: 3, child: Placeholder()),
+          ],
+        ),
+      ),
     );
   }
 }
