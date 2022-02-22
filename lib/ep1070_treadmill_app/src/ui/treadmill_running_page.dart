bcdbd2921e40080175acb8098b16430e64fdd82d
Dreamwalker <aristojeff@gmail.com>
2022-02-22 19:59:28 +0900

    Update treadmill_running_page.dart

---
+++ lib/ep1070_treadmill_app/src/ui/treadmill_running_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TreadmillRunningPage extends StatefulWidget {
+  const TreadmillRunningPage({Key? key}) : super(key: key);
+
+  @override
+  _TreadmillRunningPageState createState() => _TreadmillRunningPageState();
+}
+
+class _TreadmillRunningPageState extends State<TreadmillRunningPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
