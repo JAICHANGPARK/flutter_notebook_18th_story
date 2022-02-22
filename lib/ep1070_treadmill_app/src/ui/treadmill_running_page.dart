782bb1c02b5112a71ade66899c31443f09bc2e8e
Dreamwalker <aristojeff@gmail.com>
2022-02-22 20:15:07 +0900

    Update treadmill_running_page.dart

--- lib/ep1070_treadmill_app/src/ui/treadmill_running_page.dart
+++ lib/ep1070_treadmill_app/src/ui/treadmill_running_page.dart
@@ -19,7 +19,7 @@ class _TreadmillRunningPageState extends State<TreadmillRunningPage> {
                 flex: 5,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
+                  children: const [
                     Text(
                       "56:12",
                       style: TextStyle(
@@ -35,65 +35,200 @@ class _TreadmillRunningPageState extends State<TreadmillRunningPage> {
                     )
                   ],
                 )),
-            Expanded(flex: 5, child: Column(
-              children: [
-                Expanded(
-                  child: Row(
+            Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24),
+                  child: Column(
                     children: [
-                      Expanded(child: Column(
-                        children: [
-                          Text("6.13",style: TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                          ),),
-                          SizedBox(height: 8,),
-                          Text("Distance", style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 12
-                          ),)
-                        ],
-                      )),
-                      Expanded(child: Column(
-                        children: [
-                          Text("381.2",style: TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                          ),),
-                          SizedBox(height: 8,),
-                          Text("Calories", style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 12
-                          ),)
-                        ],
-                      )),
-                      Expanded(child: Column(
-                        children: [
-                          Text("8838",style: TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                          ),),
-                          SizedBox(height: 8,),
-                          Text("Steps", style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 12
-                          ),)
-                        ],
-                      )),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: Column(
+                              children: const [
+                                Text(
+                                  "6.13",
+                                  style: TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "Distance",
+                                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                                )
+                              ],
+                            )),
+                            Expanded(
+                                child: Column(
+                              children: const [
+                                Text(
+                                  "381.2",
+                                  style: TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "Calories",
+                                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                                )
+                              ],
+                            )),
+                            Expanded(
+                                child: Column(
+                              children: const [
+                                Text(
+                                  "8838",
+                                  style: TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "Steps",
+                                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                                )
+                              ],
+                            )),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: Column(
+                              children: const [
+                                Text(
+                                  "4.5",
+                                  style: TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "Speed",
+                                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                                )
+                              ],
+                            )),
+                            Expanded(
+                              child: Column(
+                                children: const [
+                                  Text(
+                                    "8`13",
+                                    style: TextStyle(
+                                      fontSize: 32,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text(
+                                    "Avg pace",
+                                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                children: const [
+                                  Text(
+                                    "110",
+                                    style: TextStyle(
+                                      fontSize: 32,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text(
+                                    "Cadence",
+                                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
+                )),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 48),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const CircleAvatar(
+                          radius: 24,
+                          backgroundColor: Colors.green,
+                          foregroundColor: Colors.black,
+                          child: const Icon(Icons.speed),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        const Text(
+                          "SPEED",
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const CircleAvatar(
+                          child: const Text("STOP"),
+                          radius: 38,
+                        ),
+                        SizedBox(height: 16,),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const CircleAvatar(
+                          radius: 24,
+                          backgroundColor: Colors.green,
+                          foregroundColor: Colors.black,
+                          child: const Icon(Icons.speed),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        const Text(
+                          "SPEED",
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(child: Column()),
-                      Expanded(child: Column()),
-                      Expanded(child: Column()),
-                    ],
-                  ),
-                )
-              ],
-            )),
-            Expanded(flex: 3, child: Placeholder()),
+              ),
+            ),
           ],
         ),
       ),
