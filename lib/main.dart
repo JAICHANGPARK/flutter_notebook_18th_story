c6d9f37f9d76e4cc1393e2dc7311f357cdbbc31f
Dreamwalker <aristojeff@gmail.com>
2022-02-23 19:45:38 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_notebook_18th_story/ep1070_treadmill_app/treadmill_app.dart';
+import 'ep1073_cargo_transportation/cargo_transportation.dart';
 
 
 main() {
   runApp(
      const ProviderScope(
-      child: TreadmillApp(),
+      child: CargoTransportation(),
     ),
   );
 }
