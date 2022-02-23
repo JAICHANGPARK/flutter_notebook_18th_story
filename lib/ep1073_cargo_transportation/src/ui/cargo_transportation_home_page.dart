f56a5b7b15e5aa6455fc696f05128b11162dcc8d
Dreamwalker <aristojeff@gmail.com>
2022-02-23 19:50:24 +0900

    Update cargo_transportation_home_page.dart

--- lib/ep1073_cargo_transportation/src/ui/cargo_transportation_home_page.dart
+++ lib/ep1073_cargo_transportation/src/ui/cargo_transportation_home_page.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:ionicons/ionicons.dart';
 
 class CargoTransportationHomePage extends StatefulWidget {
   const CargoTransportationHomePage({Key? key}) : super(key: key);
@@ -12,10 +13,41 @@ class _CargoTransportationHomePageState extends State<CargoTransportationHomePag
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Monday, July 1",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text("Barcelona"),
+                  ],
+                ),
+                Container(
+                  height: 32,
+                  width: 32,
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                  ),
+                  child: Icon(
+                    Ionicons.apps_outline,
+                    size: 18,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
