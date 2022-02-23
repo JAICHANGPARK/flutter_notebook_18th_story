37561a8fe03551d7778f18f4d4115a8798d07624
Dreamwalker <aristojeff@gmail.com>
2022-02-23 19:45:19 +0900

    Create cargo_transportation_home_page.dart

---
+++ lib/ep1073_cargo_transportation/src/ui/cargo_transportation_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class CargoTransportationHomePage extends StatefulWidget {
+  const CargoTransportationHomePage({Key? key}) : super(key: key);
+
+  @override
+  _CargoTransportationHomePageState createState() => _CargoTransportationHomePageState();
+}
+
+class _CargoTransportationHomePageState extends State<CargoTransportationHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
