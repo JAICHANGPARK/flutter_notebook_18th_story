188fca06d8e8f0caae34473eed042accefdf268d
Dreamwalker <aristojeff@gmail.com>
2022-02-23 19:45:00 +0900

    Update cargo_transportation.dart

--- lib/ep1073_cargo_transportation/cargo_transportation.dart
+++ lib/ep1073_cargo_transportation/cargo_transportation.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'src/ui/cargo_transportation_home_page.dart';
+
+class CargoTransportation extends StatelessWidget {
+  const CargoTransportation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CargoTransportationHomePage(),
+    );
+  }
+}
