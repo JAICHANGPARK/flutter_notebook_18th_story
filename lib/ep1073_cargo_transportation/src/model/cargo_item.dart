0113888ab2fb6afd05c5d5039bd55c4e6eea51b0
Dreamwalker <aristojeff@gmail.com>
2022-02-25 20:04:41 +0900

    Update cargo_item.dart

--- lib/ep1073_cargo_transportation/src/model/cargo_item.dart
+++ lib/ep1073_cargo_transportation/src/model/cargo_item.dart
@@ -13,3 +13,30 @@ class CargoItem with _$CargoItem {
     bool? withBed,
   }) = _CargoItem;
 }
+
+List<CargoItem> cargoItems = [
+  CargoItem(
+    time: "12:30",
+    title: "Mandarin Oriental, 38-40",
+    location: "08007 Barcelona, Spain",
+    isOut: false,
+    isTomorrow: false,
+    withBed: true,
+  ),
+  CargoItem(
+    time: "07:13",
+    title: "9 Sherif Khimshiashvill St",
+    location: "Batumi, Georgia",
+    isOut: true,
+    isTomorrow: true,
+    withBed: false,
+  ),
+  CargoItem(
+    time: "20:00",
+    title: "9 Sherif Khimshiashvill St",
+    location: "Batumi, Georgia",
+    isOut: false,
+    isTomorrow: false,
+    withBed: false,
+  ),
+];
