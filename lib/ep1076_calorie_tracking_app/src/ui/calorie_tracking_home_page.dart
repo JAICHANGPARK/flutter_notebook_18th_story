c349b10d3b6e233debc1535917e7249c65506a4e
Dreamwalker <aristojeff@gmail.com>
2022-02-26 17:08:55 +0900

    Create calorie_tracking_home_page.dart

---
+++ lib/ep1076_calorie_tracking_app/src/ui/calorie_tracking_home_page.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+
+class CalorieTrackingHomePage extends StatefulWidget {
+  const CalorieTrackingHomePage({Key? key}) : super(key: key);
+
+  @override
+  _CalorieTrackingHomePageState createState() => _CalorieTrackingHomePageState();
+}
+
+class _CalorieTrackingHomePageState extends State<CalorieTrackingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
