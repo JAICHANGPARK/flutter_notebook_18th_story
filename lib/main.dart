3f12c051574d069e3ae6688329710b5461714e6f
Dreamwalker <aristojeff@gmail.com>
2022-02-26 17:08:49 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'ep1073_cargo_transportation/cargo_transportation.dart';
+import 'ep1076_calorie_tracking_app/calorie_tracking_app.dart';
 
 
 main() {
   runApp(
      const ProviderScope(
-      child: CargoTransportation(),
+      child: CalorieTrackingApp(),
     ),
   );
 }
