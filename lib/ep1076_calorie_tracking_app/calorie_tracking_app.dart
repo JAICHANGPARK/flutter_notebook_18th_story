306a1b8689af70ddc527a34c2217dcbbecd0ccf5
Dreamwalker <aristojeff@gmail.com>
2022-02-26 17:10:26 +0900

    Update calorie_tracking_app.dart

--- lib/ep1076_calorie_tracking_app/calorie_tracking_app.dart
+++ lib/ep1076_calorie_tracking_app/calorie_tracking_app.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'src/ui/calorie_tracking_home_page.dart';
+
 
 class CalorieTrackingApp extends StatelessWidget {
   const CalorieTrackingApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: CalorieTrackingHomePage(),
+    );
   }
 }
