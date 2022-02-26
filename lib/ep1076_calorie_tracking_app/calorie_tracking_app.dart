b2d2efe743d59191c4c56606b01194d2db9e67d3
Dreamwalker <aristojeff@gmail.com>
2022-02-26 17:06:49 +0900

    Create calorie_tracking_app.dart

---
+++ lib/ep1076_calorie_tracking_app/calorie_tracking_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class CalorieTrackingApp extends StatelessWidget {
+  const CalorieTrackingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
