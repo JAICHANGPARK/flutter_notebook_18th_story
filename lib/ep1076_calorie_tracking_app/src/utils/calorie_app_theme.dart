6ceca4db49eff47c46be87c93f8c56d730daedcc
Dreamwalker <aristojeff@gmail.com>
2022-02-27 17:22:48 +0900

    Create calorie_app_theme.dart

---
+++ lib/ep1076_calorie_tracking_app/src/utils/calorie_app_theme.dart
@@ -0,0 +1,8 @@
+
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+
+Color appBarColor = Color.fromRGBO(20, 22, 29, 1);
+Color backGroundColor =Color.fromRGBO(29, 31, 41, 1);
+Color primaryColor = Color.fromRGBO(70, 131, 247, 1);
