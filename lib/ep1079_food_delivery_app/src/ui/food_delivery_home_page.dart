e82b97658c42f4a3970672cd35ef321f0e50385f
Dreamwalker <aristojeff@gmail.com>
2022-03-01 18:31:33 +0900

    Update food_delivery_home_page.dart

--- lib/ep1079_food_delivery_app/src/ui/food_delivery_home_page.dart
+++ lib/ep1079_food_delivery_app/src/ui/food_delivery_home_page.dart
@@ -9,8 +9,41 @@ class FoodDeliveryHomePage extends StatelessWidget {
       backgroundColor: Colors.black,
       body: Stack(
         children: [
+          Positioned(
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 100,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(24),
+                    bottomRight: Radius.circular(24),
+                  )),
+            ),
+          ),
+          Positioned(
+              left: 16,
+              right: 16,
+              bottom: 8,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.home),
+                    color: Colors.white,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.home),
+                    color: Colors.white,
+                  ),
 
-          
+                ],
+              ))
         ],
       ),
     );
