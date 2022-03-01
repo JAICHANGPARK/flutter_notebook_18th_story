5348861fb6973e993d5849c1607f1ed55ec299c7
Dreamwalker <aristojeff@gmail.com>
2022-03-01 18:37:06 +0900

    Update food_delivery_home_page.dart

--- lib/ep1079_food_delivery_app/src/ui/food_delivery_home_page.dart
+++ lib/ep1079_food_delivery_app/src/ui/food_delivery_home_page.dart
@@ -7,44 +7,110 @@ class FoodDeliveryHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            top: 0,
-            right: 0,
-            bottom: 100,
-            child: Container(
-              decoration: BoxDecoration(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              top: 0,
+              right: 0,
+              bottom: 72,
+              child: Container(
+                decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(24),
                     bottomRight: Radius.circular(24),
-                  )),
+                  ),
+                ),
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 58,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: const EdgeInsets.all(6),
+                      child: Row(
+                        children: [
+                          const Expanded(child: Placeholder()),
+                          const SizedBox(width: 8,),
+                          Expanded(child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(4)
+                            ),
+                          )),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 16,),
+                    Expanded(child: ListView()),
+                  ],
+                ),
+              ),
             ),
-          ),
-          Positioned(
-              left: 16,
-              right: 16,
-              bottom: 8,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.home),
-                    color: Colors.white,
+            Positioned(
+                left: 16,
+                right: 16,
+                bottom: 8,
+                child: SizedBox(
+                  height: 78,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.home),
+                        color: Colors.white,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.home),
+                        color: Colors.white,
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            height: 68,
+                            width: 68,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 8,
+                              ),
+                              color: Colors.red,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          const CircleAvatar(
+                            radius: 3,
+                            backgroundColor: Colors.white,
+                          )
+                        ],
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.home),
+                        color: Colors.white,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.home),
+                        color: Colors.white,
+                      ),
+                    ],
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.home),
-                    color: Colors.white,
-                  ),
-
-                ],
-              ))
-        ],
+                ))
+          ],
+        ),
       ),
     );
   }
