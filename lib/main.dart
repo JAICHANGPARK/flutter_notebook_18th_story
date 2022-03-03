a769c9a1fbdad587882557517fbf9639f8f478eb
Dreamwalker <aristojeff@gmail.com>
2022-03-03 20:31:23 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'ep1079_food_delivery_app/food_delivery_app.dart';
+import 'ep1081_pax_wardrobe_app/pax_wardrobe_app.dart';
 
 
 main() {
   runApp(
      const ProviderScope(
-      child: FoodDeliveryApp(),
+      child: PaxWardrobeApp(),
     ),
   );
 }
