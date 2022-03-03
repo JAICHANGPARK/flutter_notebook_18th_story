4d5170c78c3d9af145a6a602708dc280893f96e2
Dreamwalker <aristojeff@gmail.com>
2022-03-03 20:30:42 +0900

    Create pax_wardrobe_basic_page.dart

---
+++ lib/ep1081_pax_wardrobe_app/src/pax_wardrobe_basic_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class PaxWardrobeBasicPage extends StatefulWidget {
+  const PaxWardrobeBasicPage({Key? key}) : super(key: key);
+
+  @override
+  State<PaxWardrobeBasicPage> createState() => _PaxWardrobeBasicPageState();
+}
+
+class _PaxWardrobeBasicPageState extends State<PaxWardrobeBasicPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
