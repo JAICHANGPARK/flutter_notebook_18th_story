969e1185d809a4c5cd75248de67f9e46ffb081f9
Dreamwalker <aristojeff@gmail.com>
2022-03-03 20:30:01 +0900

    Create pax_wardrobe_app.dart

---
+++ lib/ep1081_pax_wardrobe_app/pax_wardrobe_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class PaxWardrobeApp extends StatelessWidget {
+  const PaxWardrobeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
