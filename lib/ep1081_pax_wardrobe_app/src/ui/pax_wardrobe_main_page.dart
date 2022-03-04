ec910fcb53893910aa08ad295a28743ef400fd8c
Dreamwalker <aristojeff@gmail.com>
2022-03-04 19:04:23 +0900

    Update pax_wardrobe_main_page.dart

--- lib/ep1081_pax_wardrobe_app/src/ui/pax_wardrobe_main_page.dart
+++ lib/ep1081_pax_wardrobe_app/src/ui/pax_wardrobe_main_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_18th_story/ep1081_pax_wardrobe_app/src/provider/pax_top_tab_index.dart';
+import 'package:flutter_notebook_18th_story/ep1081_pax_wardrobe_app/src/ui/pax_wardrobe_doors_page.dart';
+import 'package:flutter_notebook_18th_story/ep1081_pax_wardrobe_app/src/ui/pax_wardrobe_storage_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'pax_wardrobe_basic_page.dart';
 
 class PaxWardrobeMainPage extends StatelessWidget {
   const PaxWardrobeMainPage({Key? key}) : super(key: key);
@@ -132,18 +136,18 @@ class PaxWardrobeMainPage extends StatelessWidget {
             Expanded(
               child: Padding(
                 padding: EdgeInsets.all(8.0),
-                child: Consumer(builder: (BuildContext context, WidgetRef ref, Widget? child) {
-                  final idx = ref.watch(paxTopTabIndex);
-                  return IndexedStack(
-                    index: idx,
-                    children: [
-                      PaxWardrobeBasicPage(),
-                      PaxWardrobeDoorsPage(),
-                    ],
-                  );
-
-                },
-
+                child: Consumer(
+                  builder: (BuildContext context, WidgetRef ref, Widget? child) {
+                    final idx = ref.watch(paxTopTabIndex);
+                    return IndexedStack(
+                      index: idx,
+                      children: const [
+                        PaxWardrobeBasicPage(),
+                        PaxWardrobeDoorsPage(),
+                        PaxWardrobeStoragePage(),
+                      ],
+                    );
+                  },
                 ),
               ),
             ),
@@ -156,65 +160,61 @@ class PaxWardrobeMainPage extends StatelessWidget {
               padding: const EdgeInsets.all(12),
               child: Row(
                 children: [
-                  Consumer(
-                    builder: (context, ref, _) {
-                      final idx = ref.watch(paxTopTabIndex);
-                      return Container(
-                        height: 58,
-                        width: 58,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(color: Colors.grey),
+                  Consumer(builder: (context, ref, _) {
+                    final idx = ref.watch(paxTopTabIndex);
+                    return Container(
+                      height: 58,
+                      width: 58,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.grey),
+                      ),
+                      child: Center(
+                        child: IconButton(
+                          icon: const Icon(Icons.arrow_back),
+                          onPressed: () {
+                            var i = idx;
+                            i--;
+                            if (i < 1) {
+                              i = 0;
+                            }
+                            ref.watch(paxTopTabIndex.notifier).state = i;
+                          },
                         ),
-                        child: Center(
-                          child: IconButton(
-                            icon: const Icon(Icons.arrow_back),
-                            onPressed: () {
-                              var i = idx;
-                              i--;
-                              if(i < 1){
-                                i = 0;
-                              }
-                              ref.watch(paxTopTabIndex.notifier).state = i;
-                            },
-                          ),
-                        ),
-                      );
-                    }
-                  ),
+                      ),
+                    );
+                  }),
                   const Spacer(),
-                  Consumer(
-                    builder: (context, ref, _) {
-                      final idx = ref.watch(paxTopTabIndex);
-                      return GestureDetector(
-                        onTap: (){
-                          var i = idx;
-                          i++;
-                          if( i > 1){
-                            i = 2;
-                          }
-                          ref.watch(paxTopTabIndex.notifier).state = i;
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.black,
-                            borderRadius: BorderRadius.circular(32),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 64,
-                          ),
-                          child: const Center(
-                            child: Text(
-                              "Next",
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
+                  Consumer(builder: (context, ref, _) {
+                    final idx = ref.watch(paxTopTabIndex);
+                    return GestureDetector(
+                      onTap: () {
+                        var i = idx;
+                        i++;
+                        if (i > 1) {
+                          i = 2;
+                        }
+                        ref.watch(paxTopTabIndex.notifier).state = i;
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(32),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 64,
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Next",
+                            style: TextStyle(
+                              color: Colors.white,
                             ),
                           ),
                         ),
-                      );
-                    }
-                  )
+                      ),
+                    );
+                  })
                 ],
               ),
             )
