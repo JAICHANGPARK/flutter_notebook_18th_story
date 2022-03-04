88a9a201082fe4003481c228b03f284855ba5cc2
Dreamwalker <aristojeff@gmail.com>
2022-03-04 19:03:09 +0900

    Create pax_wardrobe_doors_page.dart

---
+++ lib/ep1081_pax_wardrobe_app/src/ui/pax_wardrobe_doors_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PaxWardrobeDoorsPage extends StatelessWidget {
+  const PaxWardrobeDoorsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Door UI "),
+    );
+  }
+}
