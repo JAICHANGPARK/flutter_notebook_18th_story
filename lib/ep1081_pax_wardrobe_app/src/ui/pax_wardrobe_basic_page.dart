0590cabec6d8f494ceecc1faea763e7b4ba7aa66
Dreamwalker <aristojeff@gmail.com>
2022-03-05 16:38:51 +0900

    Update pax_wardrobe_basic_page.dart

--- lib/ep1081_pax_wardrobe_app/src/ui/pax_wardrobe_basic_page.dart
+++ lib/ep1081_pax_wardrobe_app/src/ui/pax_wardrobe_basic_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class PaxWardrobeBasicPage extends StatefulWidget {
   const PaxWardrobeBasicPage({Key? key}) : super(key: key);
 
@@ -11,25 +9,45 @@ class PaxWardrobeBasicPage extends StatefulWidget {
 
 class _PaxWardrobeBasicPageState extends State<PaxWardrobeBasicPage> {
   double _widthValue = 50.0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Basic information"),
-
+        Text(
+          "Basic information",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 18
+          ),
+        ),
+        SizedBox(height: 32,),
         Text("Width"),
+        SizedBox(height: 24,),
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-
+            Text("50", style: TextStyle(
+                color: Colors.grey,
+                fontSize: 10
+            ),),
+            Text("300"),
           ],
         ),
-        Slider(value: _widthValue, onChanged: (double value) {
-
-        },
-
+        Slider(
+          max: 300,
+          min: 50,
+          value: _widthValue,
+          onChanged: (double value) {
+            setState(() {
+              _widthValue = value;
+            });
+          },
+          label: "${_widthValue.toStringAsFixed(0)} cm",
         )
       ],
-
     );
   }
 }
