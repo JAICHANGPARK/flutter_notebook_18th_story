1d5008c532009066bd3edaee62af60a995e4a17b
Dreamwalker <aristojeff@gmail.com>
2022-03-05 16:40:06 +0900

    Update pax_wardrobe_basic_page.dart

--- lib/ep1081_pax_wardrobe_app/src/ui/pax_wardrobe_basic_page.dart
+++ lib/ep1081_pax_wardrobe_app/src/ui/pax_wardrobe_basic_page.dart
@@ -12,42 +12,54 @@ class _PaxWardrobeBasicPageState extends State<PaxWardrobeBasicPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Basic information",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-            fontSize: 18
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Basic information",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 18),
           ),
-        ),
-        SizedBox(height: 32,),
-        Text("Width"),
-        SizedBox(height: 24,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("50", style: TextStyle(
-                color: Colors.grey,
-                fontSize: 10
-            ),),
-            Text("300"),
-          ],
-        ),
-        Slider(
-          max: 300,
-          min: 50,
-          value: _widthValue,
-          onChanged: (double value) {
-            setState(() {
-              _widthValue = value;
-            });
-          },
-          label: "${_widthValue.toStringAsFixed(0)} cm",
-        )
-      ],
+          SizedBox(
+            height: 32,
+          ),
+          Text("Width"),
+          SizedBox(
+            height: 24,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "50",
+                  style: TextStyle(color: Colors.grey, fontSize: 10),
+                ),
+                Text(
+                  "300",
+                  style: TextStyle(color: Colors.grey, fontSize: 10),
+                ),
+              ],
+            ),
+          ),
+          Slider(
+            max: 300,
+            min: 50,
+            value: _widthValue,
+            thumbColor: Colors.black,
+            activeColor: Colors.black,
+            inactiveColor: Colors.grey,
+            onChanged: (double value) {
+              setState(() {
+                _widthValue = value;
+              });
+            },
+            label: "${_widthValue.toStringAsFixed(0)} cm",
+          )
+        ],
+      ),
     );
   }
 }
