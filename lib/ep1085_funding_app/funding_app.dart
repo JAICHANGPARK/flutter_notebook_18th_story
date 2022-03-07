b4044e634a36e96c9ac651df9a0c682e637c725f
Dreamwalker <aristojeff@gmail.com>
2022-03-07 19:54:50 +0900

    Update funding_app.dart

---
+++ lib/ep1085_funding_app/funding_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_18th_story/ep1085_funding_app/src/ui/funding_home_page.dart';
+
+class FundingApp extends StatelessWidget {
+  const FundingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FundingHomePage(),
+    );
+  }
+}
