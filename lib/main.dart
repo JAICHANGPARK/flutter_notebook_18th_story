5b33071a8cdc36bb51e01c9ed84ae2e45fdfe115
Dreamwalker <aristojeff@gmail.com>
2022-03-07 19:58:56 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_18th_story/ep1085_funding_app/funding_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'ep1081_pax_wardrobe_app/pax_wardrobe_app.dart';
 
 
 main() {
   runApp(
      const ProviderScope(
-      child: PaxWardrobeApp(),
+      child: FundingApp(),
     ),
   );
 }
