b0f53ac7569196c5d13db8d767f9c08987016c2e
Dreamwalker <aristojeff@gmail.com>
2022-03-07 19:58:51 +0900

    Update funding_home_page.dart

--- lib/ep1085_funding_app/src/ui/funding_home_page.dart
+++ lib/ep1085_funding_app/src/ui/funding_home_page.dart
@@ -7,9 +7,31 @@ class FundingHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            
+            Positioned(
+              left: 16,
+              right: 16,
+              bottom: 16,
+              top: 16,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.arrow_back),
+                      ),
+                      Expanded(
+                        child: Center(
+                          child: Text("Calculator Zakat"),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
