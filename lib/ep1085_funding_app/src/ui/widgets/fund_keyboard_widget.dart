2bd95758e76625a08edfaa8c143e47897bbe6893
Dreamwalker <aristojeff@gmail.com>
2022-03-09 18:51:39 +0900

    Create fund_keyboard_widget.dart

---
+++ lib/ep1085_funding_app/src/ui/widgets/fund_keyboard_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FundKeyboardWidget extends StatefulWidget {
+  const FundKeyboardWidget({Key? key}) : super(key: key);
+
+  @override
+  State<FundKeyboardWidget> createState() => _FundKeyboardWidgetState();
+}
+
+class _FundKeyboardWidgetState extends State<FundKeyboardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
