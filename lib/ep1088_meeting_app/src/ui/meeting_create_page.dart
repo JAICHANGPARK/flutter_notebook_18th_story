a69c2c5950782b345456d09bd6ea7afb06322191
Dreamwalker <aristojeff@gmail.com>
2022-03-10 19:47:31 +0900

    Create meeting_create_page.dart

---
+++ lib/ep1088_meeting_app/src/ui/meeting_create_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MeetingCreatePage extends StatefulWidget {
+  const MeetingCreatePage({Key? key}) : super(key: key);
+
+  @override
+  State<MeetingCreatePage> createState() => _MeetingCreatePageState();
+}
+
+class _MeetingCreatePageState extends State<MeetingCreatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
