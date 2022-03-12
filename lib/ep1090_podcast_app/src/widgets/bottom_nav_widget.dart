0dcfb2c698c1136c36586a14400199f6449f8297
Dreamwalker <aristojeff@gmail.com>
2022-03-12 17:45:26 +0900

    Update bottom_nav_widget.dart

--- lib/ep1090_podcast_app/src/widgets/bottom_nav_widget.dart
+++ lib/ep1090_podcast_app/src/widgets/bottom_nav_widget.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_18th_story/ep1090_podcast_app/src/provider/page_index_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 class BottomNavWidget extends ConsumerWidget {
   const BottomNavWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, ref) {
     final index = ref.watch(pageIndexProvider);
-    return  BottomNavigationBar(
-      onTap: (idx){
+    return BottomNavigationBar(
+      onTap: (idx) {
         ref.watch(pageIndexProvider.notifier).state = idx;
       },
+      currentIndex: index,
       type: BottomNavigationBarType.fixed,
       items: const [
         BottomNavigationBarItem(
