2469a381a01ffdc3bacb83419c3c42c7eb438560
Dreamwalker <aristojeff@gmail.com>
2022-03-12 17:28:11 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'ep1088_meeting_app/meeting_app.dart';
+import 'ep1090_podcast_app/podcast_app.dart';
+
 
 main() {
   runApp(
     const ProviderScope(
-      child: MeetingApp(),
+      child: PodcastApp(),
     ),
   );
 }
