b10c78a2e3f2b4968b041bd78201f45004311d8e
Dreamwalker <aristojeff@gmail.com>
2022-03-12 17:27:40 +0900

    Create podcast_app.dart

---
+++ lib/ep1090_podcast_app/podcast_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class PodcastApp extends StatelessWidget {
+  const PodcastApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
