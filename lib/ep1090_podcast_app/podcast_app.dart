5c896f1999c4eee14264dac4c879729d19a1bec0
Dreamwalker <aristojeff@gmail.com>
2022-03-12 17:34:57 +0900

    Update podcast_app.dart

--- lib/ep1090_podcast_app/podcast_app.dart
+++ lib/ep1090_podcast_app/podcast_app.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-
 class PodcastApp extends StatelessWidget {
   const PodcastApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: PodMainPage(),
     );
   }
 }
