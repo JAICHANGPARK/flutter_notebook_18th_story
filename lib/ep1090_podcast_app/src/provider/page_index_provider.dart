eac7b928fe483185b9aeb4d8f89da5cf95e13834
Dreamwalker <aristojeff@gmail.com>
2022-03-13 15:18:46 +0900

    Update page_index_provider.dart

--- lib/ep1090_podcast_app/src/provider/page_index_provider.dart
+++ lib/ep1090_podcast_app/src/provider/page_index_provider.dart
@@ -6,7 +6,7 @@ final pageIndexProvider = StateProvider<int>((ref) => 0);
 final sdad = StreamProvider((ref) => TestStream().getIdx());
 
 class TestStream {
-  StreamController<int> _streamController = StreamController();
+  final StreamController<int> _streamController = StreamController();
 
   void aFunc(int i) {
     _streamController.add(i);
