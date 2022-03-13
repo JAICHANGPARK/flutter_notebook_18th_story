eb8445d3ba32693fb2a8a3905818623524a30a3e
Dreamwalker <aristojeff@gmail.com>
2022-03-13 15:21:40 +0900

    Create app_bar_widget.dart

---
+++ lib/ep1090_podcast_app/src/widgets/app_bar_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_18th_story/ep1090_podcast_app/src/provider/page_index_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+class AppBarWidget extends ConsumerWidget with PreferredSizeWidget {
+  const AppBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final idx = ref.watch(pageIndexProvider);
+    var title = "";
+    switch(idx){
+      case 0:
+        title = "Discover";
+        break;
+      default:
+        title = "Unknown";
+    }
+    return AppBar(
+      title: Text(title),
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.black,
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size();
+}
