9676ee339edf84f7aeddf4a899d4a9027ae2dc68
Dreamwalker <aristojeff@gmail.com>
2022-03-13 15:37:44 +0900

    Update discover_for_you_widget.dart

--- lib/ep1090_podcast_app/src/widgets/discover/discover_for_you_widget.dart
+++ lib/ep1090_podcast_app/src/widgets/discover/discover_for_you_widget.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 
-
 class DiscoverForYouWidget extends StatelessWidget {
   const DiscoverForYouWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(top: 16, left: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Popular Podcasts",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 8),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "SEE ALL",
+                    )),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
