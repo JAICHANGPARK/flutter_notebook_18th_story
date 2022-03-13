448f92353a02d46ecc696a9db214290f3ff50261
Dreamwalker <aristojeff@gmail.com>
2022-03-13 15:35:24 +0900

    Create discover_for_you_widget.dart

---
+++ lib/ep1090_podcast_app/src/widgets/discover/discover_for_you_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class DiscoverForYouWidget extends StatelessWidget {
+  const DiscoverForYouWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
