119e37a4a7fab43902d65f2384e4b6554d834d09
Dreamwalker <aristojeff@gmail.com>
2022-03-13 14:58:35 +0900

    Update pod_main_page.dart

--- lib/ep1090_podcast_app/src/ui/pod_main_page.dart
+++ lib/ep1090_podcast_app/src/ui/pod_main_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_18th_story/ep1090_podcast_app/src/provider/page_index_provider.dart';
 import 'package:flutter_notebook_18th_story/ep1090_podcast_app/src/widgets/bottom_nav_widget.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PodMainPage extends StatelessWidget {
   const PodMainPage({Key? key}) : super(key: key);
@@ -7,7 +9,37 @@ class PodMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavWidget(),
+      body: Consumer(
+        builder: (context, ref, _) {
+          final idx = ref.watch(pageIndexProvider);
+          switch (idx) {
+            case 0:
+              return const Center(
+                child: Text("0"),
+              );
+            case 1:
+              return const Center(
+                child: Text("1"),
+              );
+            case 2:
+              return const Center(
+                child: Text("2"),
+              );
+            case 3:
+              return const Center(
+                child: Text("3"),
+              );
+            default:
+              return Container(
+                color: Colors.red,
+                child: const Center(
+                  child: Text("Error"),
+                ),
+              );
+          }
+        },
+      ),
+      bottomNavigationBar: const BottomNavWidget(),
     );
   }
 }
