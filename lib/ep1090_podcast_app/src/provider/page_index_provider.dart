1dea758cc679cf118037f350324aff344cfb25cb
Dreamwalker <aristojeff@gmail.com>
2022-03-13 15:03:07 +0900

    Update page_index_provider.dart

--- lib/ep1090_podcast_app/src/provider/page_index_provider.dart
+++ lib/ep1090_podcast_app/src/provider/page_index_provider.dart
@@ -2,3 +2,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:async';
 
 final pageIndexProvider = StateProvider<int>((ref) => 0);
+
+final sdad = StreamProvider((ref) => testStream().getidex());
+
+class testStream {
+  StreamController<int> _streamController = StreamController();
+
+  void afunc(int i) {
+    _streamController.add(i);
+  }
+
+  Stream<int> getidex() {
+    return _streamController.stream;
+  }
+}
