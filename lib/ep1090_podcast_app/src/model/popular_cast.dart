9e16071690066ba4e9b3c8f398d64db15c5799c8
Dreamwalker <aristojeff@gmail.com>
2022-03-14 19:34:14 +0900

    Create popular_cast.dart

---
+++ lib/ep1090_podcast_app/src/model/popular_cast.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'popular_cast.freezed.dart';
+part 'popular_cast.g.dart';
+
+@freezed
+class PopularCast with _$PopularCast {
+  factory PopularCast({
+    String? img,
+    String? tag,
+    String? title,
+    String? speaker,
+  }) = _PopularCast;
+}
