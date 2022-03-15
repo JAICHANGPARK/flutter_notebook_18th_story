cef7bb6c035e21ee042f2a3d50b6e2bd3fcc9c19
Dreamwalker <aristojeff@gmail.com>
2022-03-15 20:45:50 +0900

    Update discover_episode_provider.dart

--- lib/ep1090_podcast_app/src/provider/discover_episode_provider.dart
+++ lib/ep1090_podcast_app/src/provider/discover_episode_provider.dart
@@ -3,14 +3,13 @@ import 'dart:async';
 import 'package:flutter_notebook_18th_story/ep1090_podcast_app/src/model/discover_episode.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final discoverEpisodeProvider = StreamProvider<List<DiscoverEpisode>>((ref) =>
-    DiscoverEpisodeController().getStream());
+final discoverEpisodeProvider = StreamProvider<List<DiscoverEpisode>>((ref) => DiscoverEpisodeController().getStream());
 
 class DiscoverEpisodeController {
   final StreamController<List<DiscoverEpisode>> _streamController = StreamController();
   List<DiscoverEpisode> _items = [];
 
-  DiscoverEpisodeController(){
+  DiscoverEpisodeController() {
     addStreamData();
   }
 
@@ -19,11 +18,18 @@ class DiscoverEpisodeController {
   }
 
   Future addStreamData() async {
-    for(int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++) {
       await Future.delayed(const Duration(seconds: 2));
-      _items.add(DiscoverEpisode());
+      _items.add(
+        DiscoverEpisode(
+          img: "https://cdn.pixabay.com/photo/2015/11/19/21/10/glasses-1052010__340.jpg",
+          tag: "EDUCATION",
+          title: "EP 6: How To Manage Yourself",
+          subtitle: "Roman Jade Fed 21",
+          duration: "18:02",
+        ),
+      );
       _streamController.add(_items);
     }
-
   }
 }
