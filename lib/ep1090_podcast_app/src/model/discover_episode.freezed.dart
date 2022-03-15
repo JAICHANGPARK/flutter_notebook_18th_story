db8c8102f96549347823a65dd9c44ccc87ff9053
Dreamwalker <aristojeff@gmail.com>
2022-03-15 20:28:00 +0900

    Create discover_episode.freezed.dart

---
+++ lib/ep1090_podcast_app/src/model/discover_episode.freezed.dart
@@ -0,0 +1,238 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'discover_episode.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$DiscoverEpisodeTearOff {
+  const _$DiscoverEpisodeTearOff();
+
+  _DiscoverEpisode call(
+      {String? img,
+      String? tag,
+      String? title,
+      String? subtitle,
+      String? duration}) {
+    return _DiscoverEpisode(
+      img: img,
+      tag: tag,
+      title: title,
+      subtitle: subtitle,
+      duration: duration,
+    );
+  }
+}
+
+/// @nodoc
+const $DiscoverEpisode = _$DiscoverEpisodeTearOff();
+
+/// @nodoc
+mixin _$DiscoverEpisode {
+  String? get img => throw _privateConstructorUsedError;
+  String? get tag => throw _privateConstructorUsedError;
+  String? get title => throw _privateConstructorUsedError;
+  String? get subtitle => throw _privateConstructorUsedError;
+  String? get duration => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $DiscoverEpisodeCopyWith<DiscoverEpisode> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $DiscoverEpisodeCopyWith<$Res> {
+  factory $DiscoverEpisodeCopyWith(
+          DiscoverEpisode value, $Res Function(DiscoverEpisode) then) =
+      _$DiscoverEpisodeCopyWithImpl<$Res>;
+  $Res call(
+      {String? img,
+      String? tag,
+      String? title,
+      String? subtitle,
+      String? duration});
+}
+
+/// @nodoc
+class _$DiscoverEpisodeCopyWithImpl<$Res>
+    implements $DiscoverEpisodeCopyWith<$Res> {
+  _$DiscoverEpisodeCopyWithImpl(this._value, this._then);
+
+  final DiscoverEpisode _value;
+  // ignore: unused_field
+  final $Res Function(DiscoverEpisode) _then;
+
+  @override
+  $Res call({
+    Object? img = freezed,
+    Object? tag = freezed,
+    Object? title = freezed,
+    Object? subtitle = freezed,
+    Object? duration = freezed,
+  }) {
+    return _then(_value.copyWith(
+      img: img == freezed
+          ? _value.img
+          : img // ignore: cast_nullable_to_non_nullable
+              as String?,
+      tag: tag == freezed
+          ? _value.tag
+          : tag // ignore: cast_nullable_to_non_nullable
+              as String?,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String?,
+      subtitle: subtitle == freezed
+          ? _value.subtitle
+          : subtitle // ignore: cast_nullable_to_non_nullable
+              as String?,
+      duration: duration == freezed
+          ? _value.duration
+          : duration // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$DiscoverEpisodeCopyWith<$Res>
+    implements $DiscoverEpisodeCopyWith<$Res> {
+  factory _$DiscoverEpisodeCopyWith(
+          _DiscoverEpisode value, $Res Function(_DiscoverEpisode) then) =
+      __$DiscoverEpisodeCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String? img,
+      String? tag,
+      String? title,
+      String? subtitle,
+      String? duration});
+}
+
+/// @nodoc
+class __$DiscoverEpisodeCopyWithImpl<$Res>
+    extends _$DiscoverEpisodeCopyWithImpl<$Res>
+    implements _$DiscoverEpisodeCopyWith<$Res> {
+  __$DiscoverEpisodeCopyWithImpl(
+      _DiscoverEpisode _value, $Res Function(_DiscoverEpisode) _then)
+      : super(_value, (v) => _then(v as _DiscoverEpisode));
+
+  @override
+  _DiscoverEpisode get _value => super._value as _DiscoverEpisode;
+
+  @override
+  $Res call({
+    Object? img = freezed,
+    Object? tag = freezed,
+    Object? title = freezed,
+    Object? subtitle = freezed,
+    Object? duration = freezed,
+  }) {
+    return _then(_DiscoverEpisode(
+      img: img == freezed
+          ? _value.img
+          : img // ignore: cast_nullable_to_non_nullable
+              as String?,
+      tag: tag == freezed
+          ? _value.tag
+          : tag // ignore: cast_nullable_to_non_nullable
+              as String?,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String?,
+      subtitle: subtitle == freezed
+          ? _value.subtitle
+          : subtitle // ignore: cast_nullable_to_non_nullable
+              as String?,
+      duration: duration == freezed
+          ? _value.duration
+          : duration // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_DiscoverEpisode implements _DiscoverEpisode {
+  _$_DiscoverEpisode(
+      {this.img, this.tag, this.title, this.subtitle, this.duration});
+
+  @override
+  final String? img;
+  @override
+  final String? tag;
+  @override
+  final String? title;
+  @override
+  final String? subtitle;
+  @override
+  final String? duration;
+
+  @override
+  String toString() {
+    return 'DiscoverEpisode(img: $img, tag: $tag, title: $title, subtitle: $subtitle, duration: $duration)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _DiscoverEpisode &&
+            const DeepCollectionEquality().equals(other.img, img) &&
+            const DeepCollectionEquality().equals(other.tag, tag) &&
+            const DeepCollectionEquality().equals(other.title, title) &&
+            const DeepCollectionEquality().equals(other.subtitle, subtitle) &&
+            const DeepCollectionEquality().equals(other.duration, duration));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(img),
+      const DeepCollectionEquality().hash(tag),
+      const DeepCollectionEquality().hash(title),
+      const DeepCollectionEquality().hash(subtitle),
+      const DeepCollectionEquality().hash(duration));
+
+  @JsonKey(ignore: true)
+  @override
+  _$DiscoverEpisodeCopyWith<_DiscoverEpisode> get copyWith =>
+      __$DiscoverEpisodeCopyWithImpl<_DiscoverEpisode>(this, _$identity);
+}
+
+abstract class _DiscoverEpisode implements DiscoverEpisode {
+  factory _DiscoverEpisode(
+      {String? img,
+      String? tag,
+      String? title,
+      String? subtitle,
+      String? duration}) = _$_DiscoverEpisode;
+
+  @override
+  String? get img;
+  @override
+  String? get tag;
+  @override
+  String? get title;
+  @override
+  String? get subtitle;
+  @override
+  String? get duration;
+  @override
+  @JsonKey(ignore: true)
+  _$DiscoverEpisodeCopyWith<_DiscoverEpisode> get copyWith =>
+      throw _privateConstructorUsedError;
+}
