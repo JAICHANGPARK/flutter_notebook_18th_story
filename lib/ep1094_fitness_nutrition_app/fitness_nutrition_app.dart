1a9b443799e4600cfd2a22a2a583ea1cca4fd93e
Dreamwalker <aristojeff@gmail.com>
2022-03-16 20:15:03 +0900

    Update fitness_nutrition_app.dart

--- lib/ep1094_fitness_nutrition_app/fitness_nutrition_app.dart
+++ lib/ep1094_fitness_nutrition_app/fitness_nutrition_app.dart
@@ -7,7 +7,7 @@ class FitnessNutritionApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: FitnessNutritionMainPage(),
     );
   }
