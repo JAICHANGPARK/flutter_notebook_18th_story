b055f47070785cc2a506911912c6b1835ea0e7d8
Dreamwalker <aristojeff@gmail.com>
2022-03-16 20:11:05 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_18th_story/ep1094_fitness_nutrition_app/fitness_nutrition_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'ep1090_podcast_app/podcast_app.dart';
-
 
 main() {
   runApp(
     const ProviderScope(
-      child: PodcastApp(),
+      child: FitnessNutritionApp(),
     ),
   );
 }
