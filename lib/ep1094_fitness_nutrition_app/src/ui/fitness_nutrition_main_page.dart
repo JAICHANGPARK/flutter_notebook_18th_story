b9ffaef11e590cf2563ac8fa96343c1aca848384
Dreamwalker <aristojeff@gmail.com>
2022-03-16 20:18:30 +0900

    Update fitness_nutrition_main_page.dart

--- lib/ep1094_fitness_nutrition_app/src/ui/fitness_nutrition_main_page.dart
+++ lib/ep1094_fitness_nutrition_app/src/ui/fitness_nutrition_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FitnessNutritionMainPage extends StatefulWidget {
   const FitnessNutritionMainPage({Key? key}) : super(key: key);
 
@@ -12,11 +11,34 @@ class _FitnessNutritionMainPageState extends State<FitnessNutritionMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-
-          ],
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 72,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16,),
+                SizedBox(
+                  height: 94,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16,),
+              ],
+            ),
+          ),
         ),
       ),
     );
