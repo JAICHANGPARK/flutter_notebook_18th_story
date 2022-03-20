69217d0098e13d8894d747efd74e4dab33876581
Dreamwalker <aristojeff@gmail.com>
2022-03-20 17:49:35 +0900

    Update smart_home_control_page.dart

--- lib/ep1098_smart_home_ui/src/smart_home_control_page.dart
+++ lib/ep1098_smart_home_ui/src/smart_home_control_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SmartHomeControlPage extends StatefulWidget {
   const SmartHomeControlPage({Key? key}) : super(key: key);
 
@@ -12,17 +11,31 @@ class _SmartHomeControlPageState extends State<SmartHomeControlPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 120,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-
-            ),
-          )
-
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 84,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(16),
+                    bottomRight: Radius.circular(16),
+                  )),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_back_ios_new),
+                    color: Colors.white,
+                  ),
+                  Text("")
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
