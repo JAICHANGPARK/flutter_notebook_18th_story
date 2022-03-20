2b6a350fe0bc0938bf40d0cb897aca5c4a1221d4
Dreamwalker <aristojeff@gmail.com>
2022-03-20 17:45:31 +0900

    Update smart_home_control_page.dart

---
+++ lib/ep1098_smart_home_ui/src/smart_home_control_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class SmartHomeControlPage extends StatefulWidget {
+  const SmartHomeControlPage({Key? key}) : super(key: key);
+
+  @override
+  State<SmartHomeControlPage> createState() => _SmartHomeControlPageState();
+}
+
+class _SmartHomeControlPageState extends State<SmartHomeControlPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
