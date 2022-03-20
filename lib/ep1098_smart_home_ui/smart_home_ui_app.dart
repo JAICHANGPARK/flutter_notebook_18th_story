588028c43b66aed5b0043701cf64fee42d18a78a
Dreamwalker <aristojeff@gmail.com>
2022-03-20 17:41:29 +0900

    Create smart_home_ui_app.dart

---
+++ lib/ep1098_smart_home_ui/smart_home_ui_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class SmartHomeUiApp extends StatelessWidget {
+  const SmartHomeUiApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
