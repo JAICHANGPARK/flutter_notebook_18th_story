b4ace3b7f6b9eb33d6eaceaf0477af131e26737d
Dreamwalker <aristojeff@gmail.com>
2022-03-21 20:35:40 +0900

    Update living_room_category.dart

--- lib/ep1098_smart_home_ui/src/model/living_room_category.dart
+++ lib/ep1098_smart_home_ui/src/model/living_room_category.dart
@@ -1,4 +1,18 @@
 
-class LivingRoomCategory{
+import 'package:flutter/material.dart';
 
+class LivingRoomCategory{
+  IconData iconData;
+  String title;
+
+  LivingRoomCategory(this.iconData, this.title);
 }
+
+List<LivingRoomCategory> categoryItems = [
+  LivingRoomCategory(Icons.ac_unit, "AC"),
+  LivingRoomCategory(Icons.tv, "TV"),
+  LivingRoomCategory(Icons.lightbulb, "Light"),
+
+  LivingRoomCategory(Icons.music_note, "Music"),
+  LivingRoomCategory(Icons.videocam, "CCTV"),
+];
